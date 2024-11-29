0a739a76feace9850bd92a6c8369900485a49a84
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-11-29 18:18:54 +0530

    New

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -56,26 +56,118 @@ class Media2(Document):
         indexes = ('$file_name', )
         collection_name = COLLECTION_NAME
 
+#3rd
+
+client3 = AsyncIOMotorClient(DATABASE_URI3)
+db3 = client[DATABASE_NAME]
+instance3 = Instance.from_db(db3)
+
+@instance3.register
+class Media3(Document):
+    file_id = fields.StrField(attribute='_id')
+    file_ref = fields.StrField(allow_none=True)
+    file_name = fields.StrField(required=True)
+    file_size = fields.IntField(required=True)
+    file_type = fields.StrField(allow_none=True)
+    mime_type = fields.StrField(allow_none=True)
+    caption = fields.StrField(allow_none=True)
+
+    class Meta:
+        indexes = ('$file_name', )
+        collection_name = COLLECTION_NAME
+
+#4th
+client4 = AsyncIOMotorClient(DATABASE_URI4)
+db4 = client[DATABASE_NAME]
+instance4 = Instance.from_db(db4)
+
+@instance4.register
+class Media4(Document):
+    file_id = fields.StrField(attribute='_id')
+    file_ref = fields.StrField(allow_none=True)
+    file_name = fields.StrField(required=True)
+    file_size = fields.IntField(required=True)
+    file_type = fields.StrField(allow_none=True)
+    mime_type = fields.StrField(allow_none=True)
+    caption = fields.StrField(allow_none=True)
+
+    class Meta:
+        indexes = ('$file_name', )
+        collection_name = COLLECTION_NAME
+
+#5th
+client5 = AsyncIOMotorClient(DATABASE_URI5)
+db5 = client[DATABASE_NAME]
+instance5 = Instance.from_db(db5)
+
+@instance5.register
+class Media5(Document):
+    file_id = fields.StrField(attribute='_id')
+    file_ref = fields.StrField(allow_none=True)
+    file_name = fields.StrField(required=True)
+    file_size = fields.IntField(required=True)
+    file_type = fields.StrField(allow_none=True)
+    mime_type = fields.StrField(allow_none=True)
+    caption = fields.StrField(allow_none=True)
+
+    class Meta:
+        indexes = ('$file_name', )
+        collection_name = COLLECTION_NAME
+
+#6th
+client6 = AsyncIOMotorClient(DATABASE_URI6)
+db6 = client[DATABASE_NAME]
+instance6 = Instance.from_db(db6)
+
+@instance6.register
+class Media6(Document):
+    file_id = fields.StrField(attribute='_id')
+    file_ref = fields.StrField(allow_none=True)
+    file_name = fields.StrField(required=True)
+    file_size = fields.IntField(required=True)
+    file_type = fields.StrField(allow_none=True)
+    mime_type = fields.StrField(allow_none=True)
+    caption = fields.StrField(allow_none=True)
+
+    class Meta:
+        indexes = ('$file_name', )
+        collection_name = COLLECTION_NAME
+
 async def choose_mediaDB():
     """This Function chooses which database to use based on the value of indexDB key in the dict tempDict."""
     global saveMedia
     if tempDict['indexDB'] == DATABASE_URI:
         logger.info("Using first db (Media)")
         saveMedia = Media
-    else:
+    elif tempDict['indexDB'] == DATABASE_URI2:
         logger.info("Using second db (Media2)")
         saveMedia = Media2
+    elif tempDict['indexDB'] == DATABASE_URI3:
+        logger.info("Using third db (Media3)")
+        saveMedia = Media3
+    elif tempDict['indexDB'] == DATABASE_URI4:
+        logger.info("Using fourth db (Media4)")
+        saveMedia = Media4
+    elif tempDict['indexDB'] == DATABASE_URI5:
+        logger.info("Using fifth db (Media5)")
+        saveMedia = Media5
+    elif tempDict['indexDB'] == DATABASE_URI6:
+        logger.info("Using sixth db (Media6)")
+        saveMedia = Media6
+    else:
+        logger.error("Invalid database URI specified in tempDict['indexDB']")
 
-async def save_file(media):
-    """Save file in database"""
 
-    # TODO: Find better way to get same file_id for same media to avoid duplicates
+async def save_file_in_database(media):
+    global saveMedia
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+    DATABASES = [Media, Media2, Media3, Media4, Media5, Media6]
     try:
-        if await Media.count_documents({'file_id': file_id}, limit=1):
-            logger.warning(f'{getattr(media, "file_name", "NO_FILE")} is already saved in primary DB !')
-            return False, 0
+        for db in DATABASES:
+            if await db.count_documents({'file_id': file_id}, limit=1):
+                logger.warning(f'{getattr(media, "file_name", "NO_FILE")} is already saved in a database!')
+                return False, 0
         file = saveMedia(
             file_id=file_id,
             file_ref=file_ref,
@@ -91,50 +183,30 @@ async def save_file(media):
     else:
         try:
             await file.commit()
-        except DuplicateKeyError:  
-            logger.warning(
-                f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
-            )
-
+        except DuplicateKeyError:
+            logger.warning(f'{getattr(media, "file_name", "NO_FILE")} is already saved in the database')
             return False, 0
         else:
-            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
+            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to the database')
             return True, 1
 
 
-
-async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
+async def get_search_results(query, file_type=None, max_results=8, offset=0, filter=False):
     """For given query return (results, next_offset)"""
-    if chat_id is not None:
-        settings = await get_settings(int(chat_id))
-        try:
-            if settings['max_btn']:
-                max_results = 10
-            else:
-                max_results = int(MAX_B_TN)
-        except KeyError:
-            await save_group_settings(int(chat_id), 'max_btn', False)
-            settings = await get_settings(int(chat_id))
-            if settings['max_btn']:
-                max_results = 10
-            else:
-                max_results = int(MAX_B_TN)
+
     query = query.strip()
-    #if filter:
-        #better ?
-        #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
-        #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
+
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+        raw_pattern = r'(\b|[\.\+\-_]|\s|&)' + query + r'(\b|[\.\+\-_]|\s|&)'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
-    
+        raw_pattern = query.replace(' ', r'.*[&\s\.\+\-_()\[\]]')
+
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
     except:
-        return []
+        return [], '', 0
 
     if USE_CAPTION_FILTER:
         filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
@@ -143,37 +215,62 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
 
     if file_type:
         filter['file_type'] = file_type
+    
+    # Query both collections
+    cursor_media1 = Media.find(filter).sort('$natural', -1)
+    cursor_media2 = Media2.find(filter).sort('$natural', -1)
+    cursor_media3 = Media3.find(filter).sort('$natural', -1)
+    cursor_media4 = Media4.find(filter).sort('$natural', -1)
+    cursor_media5 = Media5.find(filter).sort('$natural', -1)
+    cursor_media6 = Media6.find(filter).sort('$natural', -1)
+    
+    # Ensure offset is non-negative
+    if offset < 0:
+        offset = 0
 
-    total_results = ((await Media.count_documents(filter))+(await Media2.count_documents(filter)))
+    # Fetch files from both collections
+    files_media1 = await cursor_media1.to_list(length=35)
+    files_media2 = await cursor_media2.to_list(length=35)
+    files_media3 = await cursor_media3.to_list(length=35)
+    files_media4 = await cursor_media4.to_list(length=35)
+    files_media5 = await cursor_media5.to_list(length=35)
+    files_media6 = await cursor_media6.to_list(length=35)
 
-    #verifies max_results is an even number or not
-    if max_results%2 != 0: #if max_results is an odd number, add 1 to make it an even number
-        logger.info(f"Since max_results is an odd number ({max_results}), bot will use {max_results+1} as max_results to make it even.")
-        max_results += 1
+    total_results = len(files_media1) + len(files_media2) + len(files_media3) + len(files_media4) + len(files_media5) + len(files_media6)
+    # Interleave files from both collections based on the offset
+    interleaved_files = []
+    index_media1 = index_media2 = index_media3 = index_media4 = index_media5 = index_media6 = 0
+    while index_media1 < len(files_media1) or index_media2 < len(files_media2) or index_media3 < len(files_media3) or index_media4 < len(files_media4) or index_media5 < len(files_media5) or index_media6 < len(files_media6):
+        if index_media1 < len(files_media1):
+            interleaved_files.append(files_media1[index_media1])
+            index_media1 += 1
+        if index_media2 < len(files_media2):
+            interleaved_files.append(files_media2[index_media2])
+            index_media2 += 1
+        if index_media3 < len(files_media3):
+            interleaved_files.append(files_media3[index_media3])
+            index_media3 += 1
+        if index_media4 < len(files_media4):
+            interleaved_files.append(files_media4[index_media4])
+            index_media4 += 1
+        if index_media5 < len(files_media5):
+            interleaved_files.append(files_media5[index_media5])
+            index_media5 += 1
+        if index_media6 < len(files_media6):
+            interleaved_files.append(files_media6[index_media6])
+            index_media6 += 1
+    # Slice the interleaved files based on the offset and max_results
+    files = interleaved_files[offset:offset + max_results]
 
-    cursor = Media.find(filter)
-    cursor2 = Media2.find(filter)
-    # Sort by recent
-    cursor.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
-    # Slice files according to offset and max results
-    cursor2.skip(offset).limit(max_results)
-    # Get list of files
-    fileList2 = await cursor2.to_list(length=max_results)
-    if len(fileList2)<max_results:
-        next_offset = offset+len(fileList2)
-        cursorSkipper = (next_offset-(await Media2.count_documents(filter)))
-        cursor.skip(cursorSkipper if cursorSkipper>=0 else 0).limit(max_results-len(fileList2))
-        fileList1 = await cursor.to_list(length=(max_results-len(fileList2)))
-        files = fileList2+fileList1
-        next_offset = next_offset + len(fileList1)
+    # Calculate next offset
+    next_offset = offset + len(files)
+
+    # If there are more results, return the next_offset; otherwise, set it to ''
+    if next_offset < total_results:
+        return files, next_offset, total_results
     else:
-        files = fileList2
-        next_offset = offset + max_results
-    if next_offset >= total_results:
-        next_offset = ''
-    return files, next_offset, total_results
-
+        return files, '', total_results
+        
 async def get_bad_files(query, file_type=None, filter=False):
     """For given query return (results, next_offset)"""
     query = query.strip()
@@ -218,10 +315,29 @@ async def get_file_details(query):
     filter = {'file_id': query}
     cursor = Media.find(filter)
     filedetails = await cursor.to_list(length=1)
-    if not filedetails:
-        cursor2 = Media2.find(filter)
-        filedetails = await cursor2.to_list(length=1)
-    return filedetails
+    if filedetails:
+        return filedetails
+    cursor_media2 = Media2.find(filter)
+    filedetails_media2 = await cursor_media2.to_list(length=1)
+    if filedetails_media2:
+        return filedetails_media2
+    cursor_media3 = Media3.find(filter)
+    filedetails_media3 = await cursor_media3.to_list(length=1)
+    if filedetails_media3:
+        return filedetails_media3
+    cursor_media4 = Media4.find(filter)
+    filedetails_media4 = await cursor_media4.to_list(length=1)
+    if filedetails_media4:
+        return filedetails_media4
+    cursor_media5 = Media5.find(filter)
+    filedetails_media5 = await cursor_media5.to_list(length=1)
+    if filedetails_media5:
+        return filedetails_media5
+    cursor_media6 = Media6.find(filter)
+    filedetails_media6 = await cursor_media6.to_list(length=1)
+    if filedetails_media6:
+        return filedetails_media6
+
 
 def encode_file_id(s: bytes) -> str:
     r = b""
