207be49247064133a8eef280f92a89dbcff4a711
MALLU MOVIE WORLD <155316405+mallu-movie-world-dev1@users.noreply.github.com>
2024-12-01 16:19:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1080,6 +1080,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         tot3 = await Media3.count_documents()
         #fourth db
         tot4 = await Media4.count_documents()
+        
         total = tot1 + tot2 + tot3 + tot4 
         #users and chats
         users = await db.total_users_count()
@@ -1101,7 +1102,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         used_dbSize4 = (stats4['dataSize']/(1024*1024))+(stats4['indexSize']/(1024*1024))
         free_dbSize4 = 512-used_dbSize4
         await query.message.edit_text(
-            text=script.STATUS_TXT.format(total, users, chats, tot1, round(used_dbSize, 2), round(free_dbSize, 2), tot1, round(used_dbSize2, 2), round(free_dbSize2, 2), tot2, round(used_dbSize3, 2), round(free_dbSize3, 2), tot3, round(used_dbSize4, 2), round(free_dbSize4, 2,)),
+            text=script.STATUS_TXT.format(total, users, chats, tot1, round(used_dbSize, 2), round(free_dbSize, 2), tot2, round(used_dbSize2, 2), round(free_dbSize2, 2), tot3, round(used_dbSize3, 2), round(free_dbSize3, 2), tot4, round(used_dbSize4, 2), round(free_dbSize4, 2,)),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -1120,6 +1121,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         tot3 = await Media3.count_documents()
         #fourth db
         tot4 = await Media4.count_documents()
+        
         total = tot1 + tot2 + tot3 + tot4 
         #users and chats
         users = await db.total_users_count()
@@ -1141,7 +1143,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         used_dbSize4 = (stats4['dataSize']/(1024*1024))+(stats4['indexSize']/(1024*1024))
         free_dbSize4 = 512-used_dbSize4
         await query.message.edit_text(
-            text=script.STATUS_TXT.format(total, users, chats, tot1, round(used_dbSize, 2), round(free_dbSize, 2), tot1, round(used_dbSize2, 2), round(free_dbSize2, 2), tot2, round(used_dbSize3, 2), round(free_dbSize3, 2), tot3, round(used_dbSize4, 2), round(free_dbSize4, 2,)),
+            text=script.STATUS_TXT.format(total, users, chats, tot1, round(used_dbSize, 2), round(free_dbSize, 2), tot2, round(used_dbSize2, 2), round(free_dbSize2, 2), tot3, round(used_dbSize3, 2), round(free_dbSize3, 2), tot4, round(used_dbSize4, 2), round(free_dbSize4, 2,)),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )   
